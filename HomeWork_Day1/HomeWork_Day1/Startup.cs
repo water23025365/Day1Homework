8a88511f5380173a19edd7f1de47f074f10e2507
delphi <delphi@mail.shu.edu.tw>
2016-04-14 15:31:49 +0800

    加入專案

---
+++ HomeWork_Day1/HomeWork_Day1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeWork_Day1.Startup))]
+namespace HomeWork_Day1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
